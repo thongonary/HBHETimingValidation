52c161365ed3b83e85509f10c4d2e43114605a8d
thongonary <thongnguyen1292@gmail.com>
2017-03-21 15:42:09 +0100

    Add files from eos

--- MakeTimingMaps/python/ConfFile_cfg.py
+++ MakeTimingMaps/python/ConfFile_cfg.py
@@ -10,8 +10,8 @@ process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase1_2017_realistic', '')
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-process.MessageLogger.cerr.FwkReport.reportEvery = 10000
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(50) )
+process.MessageLogger.cerr.FwkReport.reportEvery = 10
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
@@ -21,7 +21,11 @@ process.source = cms.Source("PoolSource",
 #        'file:$CMSSW_BASE/src/HBHETimingValidation/SubmitData/cfg/phase1/step3_pi1-100_2017_realistic_3Pulse.root'
 #        'file:$CMSSW_BASE/src/HBHETimingValidation/SubmitData/cfg/phase1/step3_pi1-100_2017_realistic_3Pulse_defaultFit.root'
 #        'file:$CMSSW_BASE/src/HBHETimingValidation/SubmitData/cfg/phase1/step3_pi1-100_2017_realistic_switchPulse.root'
-        'file:$CMSSW_BASE/src/HBHETimingValidation/SubmitData/cfg/phase1/step3_pi1-100_2017_realistic_switchPulse_defaultFit.root'
+        'file:$CMSSW_BASE/src/HBHETimingValidation/SubmitData/cfg/phase1/step3_ttbar_PU50_1Pulse_MCFit.root'
+#        'root://eoscms.cern.ch//eos/cms/store/group/dpg_hcal/comm_hcal/RecoAlgos/ttbarPU50_step3_9_0_0_pre5/step3_ttbar_PU50_1Pulse_defaultFit.root'
+#        'root://eoscms.cern.ch//eos/cms/store/group/dpg_hcal/comm_hcal/RecoAlgos/ttbarPU50_step3_9_0_0_pre5/step3_ttbar_PU50_3Pulse_defaultFit.root'
+#        'root://eoscms.cern.ch//eos/cms/store/group/dpg_hcal/comm_hcal/RecoAlgos/ttbarPU50_step3_9_0_0_pre5/step3_ttbar_PU50_3Pulse_MCFit.root'
+#        'root://eoscms.cern.ch//eos/cms/store/group/dpg_hcal/comm_hcal/RecoAlgos/ttbarPU50_step3_9_0_0_pre5/step3_ttbar_PU50_switchPulse_defaultFit.root'
 
     )
 )
@@ -33,6 +37,10 @@ process.timingMaps = cms.EDAnalyzer('MakeRun2M3M2Plots')
 #process.TFileService = cms.Service('TFileService', fileName = cms.string('step3_pi1-100_2017_realistic_3Pulse.root') )
 #process.TFileService = cms.Service('TFileService', fileName = cms.string('step3_pi1-100_2017_realistic_3Pulse_defaultFit.root') )
 #process.TFileService = cms.Service('TFileService', fileName = cms.string('step3_pi1-100_2017_realistic_switchPulse.root') )
-process.TFileService = cms.Service('TFileService', fileName = cms.string('step3_pi1-100_2017_realistic_switchPulse_defaultFit.root') )
+process.TFileService = cms.Service('TFileService', fileName = cms.string('step3_ttbar_PU50_1Pulse_MCFit.root') )
+#process.TFileService = cms.Service('TFileService', fileName = cms.string('step3_ttbar_PU50_1Pulse_defaultFit.root') )
+#process.TFileService = cms.Service('TFileService', fileName = cms.string('step3_ttbar_PU50_3Pulse_defaultFit.root') )
+#process.TFileService = cms.Service('TFileService', fileName = cms.string('step3_ttbar_PU50_3Pulse_MCFit.root') )
+#process.TFileService = cms.Service('TFileService', fileName = cms.string('step3_ttbar_PU50_switchPulse_defaultFit.root') )
 
 process.p = cms.Path(process.timingMaps)
